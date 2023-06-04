8a366680cfee1a68d50d119c26efeb7b2f148016
halimasadia56 <halimasadia56@gmail.com>
2023-06-04 19:52:45 +0530

    first commit

--- project5/settings.py
+++ project5/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_DIRS=[STATIC_DIR,]
+STATICFILES_DIRS=[STATIC_DIR,]
